d8deed3d855a9cf157fee7493716583193c93a5c
JoaoR235 <joao39714@gmail.com>
2026-03-09 12:25:36 -0300

    add variaveisOperadores.

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,19 @@
+let A = 8
+let B = 27
+let C = A + B;
+console.log(`Resultado da Soma entre ${A} e ${B} = ${C}`);
+
+A = 23;
+B = 12;
+C = A - B;
+console.log(`Resultado da Subtração entre ${A} e ${B} = ${C}`);
+
+A = 7;
+B = 17;
+C = A * B;
+console.log(`Resultado da Multiplicação entre ${A} e ${B} = ${C}`);
+
+A = 28;
+B = 7;
+C = A/B;
+console.log(`Resultado da Divisão entre ${A} e ${B} = ${C}`);
