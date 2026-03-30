6c1fbb963394aa02ec51d987354d1e1a532d9313
JoaoR235 <joao39714@gmail.com>
2026-03-30 20:40:31 -0300

    add appweb_calculadora.

--- appweb_calculadora/index.js
+++ appweb_calculadora/index.js
@@ -1,19 +1,36 @@
 // Carrega framework express
 const express = require('express');
 
+const calculadora = require('./calculadora')
+
 // Cria um app usando a função express()
 const app     = express();
 
 const PORT = 8080;
 
-app.get('/', function(req, res){
+const LOG = [];
+
+app.get('/', (req, res) => {
     res.send('<h1>Olá, mundo!</h1> <p>estou na web...</p>');
 });
 
-app.get('/bem-vindo', function(req, res){
-    res.send('<h1>Bem vindo ao desenvolvimeto web</h1>');
+app.get('/calculadora/somar/:a/:b', (req, res) => {
+    let a = Number(req.params.a)
+    let b = Number(req.params.b)
+    let resultado = calculadora.somar(a, b);
+    res.send(`<h1>${a} + ${b} = ${resultado}<h1>`);
+    LOG.push(string_resultado);
+    res.send(string_resultado);
+});
+
+app.get('/calculadora/log', (req, res) => {
+    resultado = "";
+    LOG.forEach(log => {
+        resultado += LOG;
+    })
+    res.send(resultado)
 });
 
 app.listen(PORT, () => {
     console.log('app rodando na porta' + PORT)
-})
+});
